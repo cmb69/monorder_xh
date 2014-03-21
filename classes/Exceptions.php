142f8038c67df3a8109b8d2ce8577d4f6ac9edb1
cmb69 <cmbecker69@gmx.de>
2014-03-21 20:24:22 +0000

    checked in Exceptions.php

---
+++ classes/Exceptions.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * The exception classes.
+ *
+ * PHP version 5
+ *
+ * @category  CMSimple_XH
+ * @package   Monorder
+ * @author    Christoph M. Becker <cmbecker69@gmx.de>
+ * @copyright 2014 Christoph M. Becker <http://3-magi.net/>
+ * @license   http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
+ * @version   SVN: $Id$
+ * @link      http://3-magi.net/?CMSimple_XH/Monorder_XH
+ */
+
+/**
+ * The exception class.
+ *
+ * @category CMSimple_XH
+ * @package  Monorder
+ * @author   Christoph M. Becker <cmbecker69@gmx.de>
+ * @license  http://www.gnu.org/licenses/gpl-3.0.en.html GNU GPLv3
+ * @link     http://3-magi.net/?CMSimple_XH/Monorder_XH
+ */
+class Monorder_Exception extends Exception
+{
+    // skip
+}
+
+?>
